42459e57d401010915042bf4e31ac9f2361ef852
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-05-29 10:46:12 +0800

    git init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicTextFieldDemo() {
+    var textState by remember { mutableStateOf(TextFieldValue("Hello World")) }
+    Column {
+        BasicTextField(value = textState, onValueChange = {
+            textState = it
+        })
+        Text("The textfield has this text: " + textState.text)
+    }
+    /* I var value by remember { mutableStateOf("") }
+    BasicTextField(
+        modifier = Modifier.border(1.dp, Color.Red).padding(4.dp),
+        value = value,
+        onValueChange = { value = it },
+        textStyle = TextStyle(
+            fontFamily = FontFamily(
+                Font(R.font.circular_std_medium_500, FontWeight.Normal),
+            ),
+            fontSize = 16.sp,
+            lineHeight = 20.sp,
+            lineHeightStyle = LineHeightStyle(
+                LineHeightStyle.Alignment.Center,
+                LineHeightStyle.Trim.None
+            ),
+            platformStyle = PlatformTextStyle(includeFontPadding = false),
+        ),
+    )
+     */
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
